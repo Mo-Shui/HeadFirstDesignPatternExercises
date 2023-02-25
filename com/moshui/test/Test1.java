9c36d4850da5618c61a30a3481fdbf4a8ff379bb
moshui <2449952049@qq.com>
2023-02-25 10:23:40 +0800

    项目初始化和helloworld测试

---
+++ com/moshui/test/Test1.java
@@ -0,0 +1,16 @@
+package com.moshui.test;
+
+import org.junit.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+        System.out.println("111");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("222");
+    }
+
+}
